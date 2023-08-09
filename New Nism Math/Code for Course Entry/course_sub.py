0dfe33e718cf0443329dda30359627be334e5a9a
David Liu <134629870+davidliu2324@users.noreply.github.com>
2023-08-09 15:35:57 -0400

    Add files via upload

--- New Nism Math/Code for Course Entry/course_sub.py
+++ New Nism Math/Code for Course Entry/course_sub.py
@@ -5,10 +5,7 @@ def input_function():
     progress = 1 #To keep track of the nth course that the user has entered so far
     progress_string = '1st'
     list_progress = 0
-
-    #course outlines are separated by main sections and sub-sections, for example in Calculus 1, a main section would be 'integrals' and a sub-section would be 'integration by parts'
-    main_sections = []
-    sub_sections = []
+    course_section_html = ''
 
     #exits are for ending the loop in the submission process
     main_exit = False
@@ -16,9 +13,10 @@ def input_function():
     course_title = str(input('Enter the name of the course '))
 
     while not main_exit:
+        sub_sections = []
         sub_section_exit = False
         course_main_section = str(input(f'Enter the {progress_string} section of the course: '))
-        main_sections.append(course_main_section)
+        course_section_html += f'<a href="#" class="topic-btn fourth-heading">{course_main_section}</a>\n<ul class="topic-list">\n'
 
         while not sub_section_exit:
             sub_section_entry = str(input("Enter a sub-section (in order and one at a time): "))
@@ -26,6 +24,10 @@ def input_function():
             sub_exit_question = str(input("Is this the last sub-section of this main section? (enter 'y' or 'n): "))
             sub_exit_question = sub_exit_question.upper()
             if sub_exit_question == 'Y': #exiting out of the sub_section
+                for j in range(len(sub_sections)):
+                        course_section_html += f'<li class="topic-list-item"><a href="#" class="list-item-link">{sub_sections[j]}</a></li>'
+                        if j == len(sub_sections) - 1:
+                            course_section_html += '\n</ul>'
                 exit_question = str(input("Is this the last section? (enter 'y' or 'n'): "))#exiting out of the main_section
                 exit_question = exit_question.upper()
                 if exit_question == 'Y':
@@ -35,32 +37,27 @@ def input_function():
                 progress += 1
                 progress_string = str(progress)+"th"
                 sub_section_exit = True
-            elif sub_exit_question == 'N':
+            elif sub_exit_question == 'N': #Not exiting out of the sub_section
                 sub_section_exit = False
-    return course_title, main_sections, sub_sections
+    return course_title, course_section_html
 
 
-def code_converter(course_name, main_section_list, sub_section_list): #Converting both main and sub sections lists into html code.
-    course_title_code = f'<h3 class="course-title heading-secondary">{course_name}</h3>\n'
+def code_converter(course_name, given_html): #Converting both main and sub sections lists into html code.
     main_sub_code = ''
+    course_title_code = f'<div class="course-title">\n<h3 class="course-title heading-secondary">{course_name}</h3>\n</div>\n'
+    main_sub_code += given_html
     closing_code = '\n</div>\n</div>\n</section>'
-    for i in range(len(main_section_list)):
-        main_sub_code += f'<a href="#" class="topic-btn fourth-heading">{main_section_list[i]}</a>\n<ul class="topic-list">\n'
-        for j in range(len(sub_section_list)):
-            main_sub_code += f'<li class="topic-list-item"><a href="#" class="list-item-link">{sub_section_list[j]}</a></li>'
-            if j == len(sub_section_list) - 1:
-                main_sub_code += '\n</ul>'
-
     final_code = f'<section>\n{course_title_code}<div class="course-content">\n<div class="course-topic">\n{main_sub_code}\n{closing_code}'
     return final_code
 
+
 def file_exporter(x):
-    file = open('Code for Course Entry\code_file.txt','w')
+    file = open('code_file.txt','w')
     file.write(x)
 
 def main():
-    course_title, main_sections, sub_sections = input_function()
-    converted_code = code_converter(course_title,main_sections,sub_sections)
+    course_title, course_section_html = input_function()
+    converted_code = code_converter(course_title,course_section_html)
     file_exporter(converted_code)
 main()
 #All Authored By NISM (David Liu)

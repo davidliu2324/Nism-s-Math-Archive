7e0bdf157d55160cbfee78ea1ba557c279ee11fe
David Liu <134629870+davidliu2324@users.noreply.github.com>
2023-08-19 13:32:37 -0400

    Delete New Nism Math directory

--- New Nism Math/test.py
+++
@@ -1,6 +0,0 @@
- while not sub_section_exit:
-            sub_section_entry = str(input("Enter a sub-section (in order and one at a time): "))
-            sub_sections.append(sub_section_entry)
-            sub_exit_question = str(input("Is this the last sub-section of this main section? (enter 'y' or 'n): "))
-            sub_exit_question = sub_exit_question.upper()
-            while 
